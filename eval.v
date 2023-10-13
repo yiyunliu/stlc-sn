3b35c4b2e7fdc67375ebd2c1a0422cef85c0a509
Yiyun Liu <liuyiyun@seas.upenn.edu>
2023-10-13 16:14:15 -0400

    Add well-scoped ap and eval

--- eval.v
+++ eval.v
@@ -6,59 +6,29 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
-Inductive Domain : Type :=
-| Clos n : tm (S n) -> (fin n -> Domain) -> Domain.
+(* An attempt at mechanizing Nbe *)
 
-Arguments Clos {n}.
+Inductive Domain (n : nat) : Type :=
+| D_Clos m : tm (S m) -> (fin m -> Domain n) -> Domain n
+| D_Ne (e : DomainNe n) : Domain n
+with DomainNe (n : nat) : Type :=
+| DN_var (i : fin n) : DomainNe n
+| DN_App (d : DomainNe n) (e : Domain n) : DomainNe n.
 
-Definition Env n := fin n -> Domain.
+Arguments D_Clos {n m}.
 
-Inductive Eval {n} : Env n -> tm n -> Domain -> Prop :=
-| EvalVar ρ i : Eval ρ (var_tm i) (ρ i)
-| EvalAbs A a ρ : Eval ρ (Lam A a) (Clos a ρ)
-| EvalApp m ρ a b a0 (ρ0 : Env m) b0 c :
-  Eval ρ a (Clos a0 ρ0) ->
-  Eval ρ b b0 ->
-  Eval (b0 .: ρ0) a0 c ->
-  Eval ρ (App a b) c.
+Definition Env n m := fin n -> Domain m.
 
-Fixpoint Interp (A : ty) (v : Domain) : Prop :=
-  match A with
-  | I => False
-  | Fun A B =>
-      match v with
-        Clos a ρ => forall v0, Interp A v0 -> exists v1, Eval (v0 .: ρ) a v1  /\ Interp B v1
-      end
-  end.
-
-Definition valuation {n} (ρ : Env n) (Γ : context n) :=
-  forall i, Interp (Γ i) (ρ i).
-
-Lemma valuation_cons {n} (ρ : Env n) (Γ : context n) (v : Domain) (A : ty)
-  (h0 : valuation ρ Γ)
-  (h1 : Interp A v) :
-  valuation (v .: ρ) (A .: Γ).
-Proof. hauto q:on unfold:valuation inv:option. Qed.
-
-#[export]Hint Resolve valuation_cons : core.
-
-Definition SemWt {n} (Γ : context n) a A :=
-  forall ρ, valuation ρ Γ -> exists v, Eval ρ a v /\ Interp A v.
-
-#[export]Hint Unfold valuation SemWt : core.
-
-Theorem fundamental_lemma {n} (Γ : context n) a A
-  (h : Wt Γ a A) :
-  SemWt Γ a A.
-Proof.
-  elim : n Γ a A / h.
-  - sfirstorder.
-  - move => n Γ A a B h0 h1 ρ hρ.
-    exists (Clos a ρ).
-    sfirstorder.
-  - rewrite /SemWt.
-    move =>  n Γ a A B b h0 ih0 h1 ih1 ρ hρ.
-    move /(_ ρ hρ) in ih0.
-    move /(_ ρ hρ) in ih1.
-    hauto q:on ctrs:Eval use:valuation_cons.
-Qed.
+Inductive D_Ap {n} : Domain n -> Domain n -> Domain n -> Prop :=
+| DAp_AppNeu e d :
+  D_Ap (D_Ne e) d (D_Ne (DN_App e d))
+| DAp_App m a (ρ : Env m n) d0 d1 :
+  D_Eval (d0 .: ρ) a d1 ->
+  D_Ap (D_Clos a ρ) d0 d1
+with D_Eval {n} : forall {m}, Env m n -> tm m -> Domain n -> Prop :=
+| DEval_Abs m A a (ρ : Env m n) : D_Eval ρ (Lam A a) (D_Clos a ρ)
+| DEval_App m a b d0 d1 d2 (ρ : Env m n) :
+  D_Eval ρ a d0 ->
+  D_Eval ρ b d1 ->
+  D_Ap d0 d1 d2 ->
+  D_Eval ρ (App a b) d2.
